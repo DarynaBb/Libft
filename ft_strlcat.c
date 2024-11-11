fc4cb2b17e30c7ee10c383d7dd7c28c1030a8a42
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2024-11-11 16:49:12 +0100

    Add files via upload
    strlcat added

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+    size_t dst_len = 0;
+    size_t src_len = 0;
+    int i = 0;
+    int k;
+
+    while (dst[dst_len] != '\0' && dst_len < dstsize)
+    {
+        dst_len++;
+    }
+
+    k = dst_len;
+
+    while (src[src_len] != '\0')
+    {
+        src_len++;
+    }
+
+    if (dst_len >= dstsize)
+    {
+        return (dstsize + src_len);
+    }
+
+    while (k < dstsize - 1 && src[i] != '\0')
+    {
+        dst[k] = src[i];
+        printf("%c", dst[k]);
+        k++;
+        i++;
+    }
+
+    dst[k] = '\0';
+
+    return (dst_len + src_len);
+}
