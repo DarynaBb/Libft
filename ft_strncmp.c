553200b633f5c7ddab49106da53e119c7a22832a
DarynaBb <darinabilenko@gmail.com>
2024-11-12 15:22:16 +0100

    strncmp func added

---
+++ ft_strncmp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    int i = 0;
+    if (n > 0) 
+    {
+        while (n > i)
+        {
+            if (s1[i] != s2[i])
+            {
+                return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+            }
+            if (s1[i] == '\0' && s2[i] == '\0')
+            {
+                return (0);
+            }
+            i++;
+        }
+    }
+    return 0;
+}
+
+// int main() {
+//     const char str1[] = "OKi";
+//     const char str2[] = "OKk";
+//     size_t n = 4;
+//     printf("%d", ft_strncmp(str1, str2, n));
+
+//     return 0;
+// }
