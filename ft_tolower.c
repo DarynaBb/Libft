1bb108b5b9cea4116638a4888022fc349f100165
DarynaBb <darinabilenko@gmail.com>
2024-11-12 17:01:27 +0100

    toupper & tolower func added

---
+++ ft_tolower.c
@@ -0,0 +1,17 @@
+
+int ft_tolower(int ch)
+{
+    if (ch >= 65 && ch <= 90)
+    {
+        ch = ch + 32;
+    }
+    return ch;
+}
+
+// #include <stdio.h>
+// int main() {
+//     char c = 'A';
+//     printf("%d", ft_tolower(c));
+
+//     return 0;
+// }
---
+++ ft_toupper.c
@@ -0,0 +1,17 @@
+
+int ft_toupper(int ch)
+{
+    if (ch >= 97 && ch <= 122)
+    {
+        ch = ch - 32;
+    }
+    return ch;
+}
+
+// #include <stdio.h>
+// int main() {
+//     char c = 'a';
+//     printf("%d", ft_toupper(c));
+
+//     return 0;
+// }
