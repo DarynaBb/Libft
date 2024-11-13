de0dcb3839f7a7b8bb1e0f8cabe27541f3727d6e
DarynaBb <darinabilenko@gmail.com>
2024-11-13 14:47:30 +0100

    strnstr fully added

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    if (needle == "")
+    if (needle == '\0')
     {
-        return (char *)haystack;
+        return ((char *)haystack);
     }
-    
-    int i = 0;
-    int k = 0;
-    
+    size_t i;
+    size_t j;
+
+    i = 0;
     while (haystack[i] != '\0' && i < len)
     {
-        if (haystack[i] == needle[0])
+        j = 0;
+        while (i + j < len && haystack[i+j] == needle[j])
         {
-            while(needle[k] != '\0' || haystack[i] == needle[k])
-        {
-            if (haystack[i] == needle[k])
+            j++;
+            if (needle[i] == '\0')
             {
-                k++;
-                i++;
-            } 
-        }
+                return ((char *)&haystack[i]);
+            }
         }
         i++;
     }
+    return (NULL);
 }
 
-int main() {
-    char c[] = "hello world hi";
-    char p[] = "ello";
-    size_t len = 3;
-    // printf("%s\n", strnstr(c, p, len));
-    printf("%s", ft_strnstr(c, p, len));
 
-    return 0;
-}
+// int main() {
+//     char c[] = "hello world hi";
+//     char p[] = "ello";
+//     size_t len = 3;
+//     printf("%s\n", strnstr(c, p, len));
+//     printf("%s", ft_strnstr(c, p, len));
+
+//     return 0;
+// }
