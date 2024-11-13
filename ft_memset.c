11ff78944668d57c40facdc5835d5a52e85d3851
DarynaBb <darinabilenko@gmail.com>
2024-11-13 16:26:38 +0100

    memset added

---
+++ ft_memset.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void *memset(void *ptr, int value, size_t num)
+{
+    size_t  i;
+
+    i = 0;
+    while (i < num)
+    {
+        ((unsigned char*)ptr)[i] = (unsigned char)value;
+        i++;
+    }
+    return (ptr);
+}
