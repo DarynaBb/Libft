c6cfb00f12293c236d27254e78911293910aa5eb
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2024-11-14 14:41:10 +0100

    memcmp added

---
+++ ft_memcmp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    unsigned char *p1 = (unsigned char *)s1;
+    unsigned char *p2 = (unsigned char *)s2;
+
+    size_t i = 0;
+
+    while (i < n)
+    {
+        if (p1[i] != p2[i])
+        {
+            return (p1[i] - p2[i]);
+        }
+        i++;
+    }
+    return (0);
+}
+
+// int main() {
+//     char str1[] = "abcdef";
+//     char str2[] = "abcdeg";
+
+//     int result = ft_memcmp(str1, str2, 6);
+
+//     if (result == 0) {
+//         printf("The first 6 bytes are equal.\n");
+//     } else if (result < 0) {
+//         printf("str1 is lexicographically smaller than str2.\n");
+//     } else {
+//         printf("str1 is lexicographically greater than str2.\n");
+//     }
+
+//     return 0;
+// }
