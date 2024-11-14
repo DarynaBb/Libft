c98075d2f05ed30ed0ac26cf939012f7223bc541
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2024-11-14 12:51:12 +0100

    Add files via upload
    memchr added

---
+++ ft_memchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void *memchr(const void *s, int c, size_t n)
+{
+    unsigned char *ptr = (unsigned char *)s;
+    unsigned char ch = (unsigned char) c;
+    size_t i;
+    i = 0;
+
+
+    while (i < n)
+    {
+        if (ptr[i] == ch)
+        {
+            return (ptr + i);
+        }
+        i++;
+    }
+    return (NULL);
+}
