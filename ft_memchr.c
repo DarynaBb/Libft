8aa63b82e726160c3ac7f87e8ab3adefbfa40ddd
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2024-11-14 12:53:32 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void *memchr(const void *s, int c, size_t n)
+void *ft_memchr(const void *s, int c, size_t n)
 {
     unsigned char *ptr = (unsigned char *)s;
     unsigned char ch = (unsigned char) c;
