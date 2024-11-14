92c1243627f078898bf9ce2ff56e35a6d42bd02b
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2024-11-14 15:18:32 +0100

    Add files via upload
    calloc func added

---
+++ ft_calloc.c
@@ -0,0 +1,34 @@
+
+#include <stdlib.h>
+
+void ft_bzero(void *s, size_t n)
+{
+    size_t  i;
+
+    i = 0;
+    while (i < n)
+    {
+        ((unsigned char *)s)[i] = 0;
+        i++;
+    }
+}
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+    void *ptr;
+
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+    {
+        return (NULL);
+    }
+    if (nmemb != 0 && size != 0)
+    {
+        ft_bzero(ptr, nmemb * size);
+    } else 
+    {
+        return (NULL);
+    }
+    return (ptr);
+}
+
