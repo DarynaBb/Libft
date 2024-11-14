4a2dbb206cd03b769ec6cdf5ec8cbe416e2033be
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2024-11-14 12:15:13 +0100

    Add files via upload
    memmove added

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+   size_t   i;
+   unsigned char *d; 
+   unsigned char *s; 
+
+    *d = (unsigned char *)dest;
+    *s = (unsigned char *)src;
+    
+
+   if (d > s && d < s + n)
+   {
+    i = n;
+    while (i > 0) {
+        i--;
+        d[i] = s[i];
+    }
+   } else 
+   {
+    i = 0;
+    while (i < n)
+    {
+        d[i] = s[i];
+        i++;
+    }
+    return (dest);
+   }
+}
