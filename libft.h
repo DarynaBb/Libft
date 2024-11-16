ea67db7905ba4e18ad057841457ef5d9efd56bdc
DarynaBb <darinabilenko@gmail.com>
2024-11-16 16:39:27 +0100

    putnbr_fd func added

--- libft.h
+++ libft.h
@@ -16,7 +16,7 @@ int ft_isdigit(int c);
 int ft_isprint(int c);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void *ft_memcpy(void *dst, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 void *ft_memset(void *ptr, int value, size_t num);
 char *ft_strchr(const char *str, int c);
