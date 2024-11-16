e299f4ba491abf0cc57e682435c7a2f93704f224
DarynaBb <darinabilenko@gmail.com>
2024-11-16 15:36:14 +0100

    striteri func added

---
+++ ft_striteri.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    int i;
+    
+    i = 0;
+    while (s[i])
+    {
+       f(i, &s[i]);
+       i++;
+    }
+}
+
+// void to_upper(unsigned int index, char *ch)
+// {
+//     if (*ch >= 'a' && *ch <= 'z')
+//         *ch -= 32; 
+// }
+
+// int main() {
+//     char s[] = "hello";
+//     ft_striteri(s, to_upper);
+    
+//     return 0;
+// }
