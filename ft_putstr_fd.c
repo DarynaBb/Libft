93c6930ada4e06ee2ccdc059605dd79963fbd9a0
DarynaBb <darinabilenko@gmail.com>
2024-11-16 15:53:11 +0100

    putstr_fd func added

---
+++ ft_putstr_fd.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+    while(*s)
+    {
+        write(fd, s, 1);
+        s++;
+    }
+}
+
+// int main() {
+//     char s[] = "hello world";
+//     ft_putstr_fd(s, 1);
+    
+//     return 0;
+// }
