c5343c2a70e438788822d7c480545a144eb18da0
DarynaBb <darinabilenko@gmail.com>
2024-11-17 20:43:42 +0100

    atoi func fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,33 +2,24 @@
 
 int ft_atoi(const char *str)
 {
-    int i = 0;
-    int sign = 1;
-    int num = 0;
-    
-    while (str[i] != '\0')
+    int i;
+    int sign;
+    int num;
+
+    i = 0;
+    sign = 1;
+    num = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if ((str[i] == '-' || str[i] == '+'))
     {
-        while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-        {
-            i++;
-        }
-        if (str[i] == '-' || str[i] == '+')
-        {
-            if(str[i] == '-')
-            {
-                sign = -1;
-            }
-            i++;
-        }
-        if (str[i] >= 48 && str[i] <= 57)
-        {
-            num *= 10;
-            num += (str[i] - '0');
-            
-        } else 
-        {
-            return (num*sign);
-        }
+        if(str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= 48 && str[i] <= 57)
+    {
+        num = num * 10 + (str[i] - '0');
         i++;
     }
     return (num*sign);
