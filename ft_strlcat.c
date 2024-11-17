0e9f1712c309e18be3606bc60bc6026aba07061c
DarynaBb <darinabilenko@gmail.com>
2024-11-17 20:00:01 +0100

    strlcat fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,32 +1,25 @@
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize) 
 {
-    size_t dst_len = 0;
-    size_t src_len = 0;
-    size_t i = 0;
-    size_t k;
+    size_t dst_len;
+    size_t src_len;
+    size_t i;
 
-    while (dst[dst_len] != '\0' && dst_len < dstsize)
-    {
-        dst_len++;
-    }
-    k = dst_len;
+    dst_len = 0;
+    src_len = 0;
+    i = 0;
     while (src[src_len] != '\0')
-    {
         src_len++;
-    }
-    if (dst_len >= dstsize)
-    {
-        return (dstsize + src_len);
-    }
-    while (k < dstsize - 1 && src[i] != '\0')
-    {
-        dst[k] = src[i];
-        printf("%c", dst[k]);
-        k++;
+    while (dst_len < dstsize && dst[dst_len] != '\0')
+        dst_len++;
+    if (dstsize <= dst_len)
+        return dstsize + src_len;
+    while (dst_len + i < dstsize - 1 && src[i] != '\0') {
+        dst[dst_len + i] = src[i];
         i++;
     }
-    dst[k] = '\0';
-    return (dst_len + src_len);
+    if (dst_len + i < dstsize)
+        dst[dst_len + i] = '\0';
+    return dst_len + src_len;
 }
